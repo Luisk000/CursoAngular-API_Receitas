d6023773561e9d95d4bdd04c8d1f5935c7de0a71
dev1@senhorcaixa.com.br <dev1@senhorcaixa.com.br>
2021-12-17 16:59:46 -0300

    Adicionar arquivos de projeto.

---
+++ routing-start/src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { ServerResolverService } from './servers/server/server-resolver.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-service.service';
+import { AuthGuardService } from './auth-guard.service';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
+import { UsersComponent } from "./users/users.component";
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent, children: [
+    { path: ':id/:name', component: UserComponent }
+  ]},
+  { path: 'servers', canActivateChild: [AuthGuardService], component: ServersComponent, children: [
+    { path: ':id', component: ServerComponent, resolve: {server: ServerResolverService} },
+    { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
+  ]},
+  /* { path: 'not-found', component: PageNotFoundComponent }, */
+  { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found'} },
+  { path: '**', redirectTo: '/not-found', pathMatch: 'full'}  // ** significa que o link engloba todas as rotas não conhecidas
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes), /* {useHash: true} */],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
---
+++ routing-start/src/app/servers/edit-server/can-deactivate-service.service.ts
@@ -0,0 +1,13 @@
+import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+
+export interface CanComponentDeativate {
+  canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
+}
+
+export class CanDeactivateGuard implements CanDeactivate<CanComponentDeativate> {
+  canDeactivate(component: CanComponentDeativate, currentRoute: ActivatedRouteSnapshot, currentState: RouterStateSnapshot, nextState?: RouterStateSnapshot): 
+  Observable<boolean> | Promise<boolean> | boolean {
+      return component.canDeactivate();
+  }
+}
