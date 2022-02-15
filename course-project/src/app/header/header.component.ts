49dc5c44cdc9466802a122eaec9a46ec4d029d15
dev1@senhorcaixa.com.br <dev1@senhorcaixa.com.br>
2022-02-15 13:29:23 -0300

    Audit Log no back-end gerenciador de perfis

---
+++ course-project/src/app/header/header.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+
+import { DataStorageService } from '../shared/data-storage.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  private userSubscription: Subscription;
+  isAuthenticated = false;
+
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
+
+  ngOnInit(){
+    this.userSubscription = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !user ? false : true;  //!!user
+      console.log(!user);
+      console.log(!!user);
+    });
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
+  }
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
+
+  onFetchData() {
+    this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  onLogout(){
+    this.authService.logout();
+  }
+}
